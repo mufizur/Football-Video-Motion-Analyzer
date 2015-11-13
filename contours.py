ede67ff1f5156f9a0dd4bd00917980ac0e26ab49
Omar <omar.khalid.yahya@gmail.com>
2015-11-13 17:52:41 +0800

    Added contour tracking method.

--- contours.py
+++ contours.py
@@ -11,11 +11,6 @@ shi_params = dict( maxCorners = 3000,
                        minDistance = 7,
                        blockSize = 7 )
 
-#find params for Lucas-Kanade optical flow
-lk_params = dict( winSize  = (12,12), #larger window means better tracking, but slower computation (more cost)
-                  maxLevel = 0,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-
 #random color gen!!! -- color[i].tolist()
 #color = np.random.randint(0,255,(1000000,3))
 
@@ -25,7 +20,7 @@ ret, old_frame = cap.read()
 #grayscale convert it
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_RGB2GRAY)
 
-writer = cv2.VideoWriter('result.avi',
+writer = cv2.VideoWriter('contours.avi',
 							 int(cap.get(cv2.cv.CV_CAP_PROP_FOURCC)),
 							 int(cap.get(cv2.cv.CV_CAP_PROP_FPS)),
 							 (int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
@@ -34,13 +29,12 @@ writer = cv2.VideoWriter('result.avi',
 index = 0
 while(index < 300):
 	print index
-	index+=1
 	#we need to continuously find good params to track
 	p_old = cv2.goodFeaturesToTrack(old_gray, mask = None, **shi_params)
 	
-	'''p_old = []
+	_, writeframe = cap2.read()
+	centroids = []
 	temp = []
-	temp2 = []
 	contours, hierarchy = cv2.findContours(old_gray, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
 	for c in contours:
 		if cv2.contourArea(c) < 150:
@@ -51,46 +45,24 @@ while(index < 300):
 		x = int(moment['m10'] / moment['m00'])
 		y = int(moment['m01'] / moment['m00'])
 		temp = [[x,y]]
-		p_old.append(temp)
+		cv2.circle(writeframe, (x, y), 10, (255,0,0), -1)
+		centroids.append(temp)
 		
-	p_old = np.array(p_old, np.float32)'''	
+	centroids = np.array(p_old, np.float32)
 
     #read curr frame and grayscale it
 	ret,frame = cap.read()
-	_, writeframe = cap2.read()
 	fgray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    
-    #init p_new in case we wanted to use flags
-	p_new = np.zeros_like(p_old)
-    
-    #use Lucas-Kanade algorithm to find optical flow
-	p_new, status, error = cv2.calcOpticalFlowPyrLK(old_gray, fgray, p_old, p_new, **lk_params)
-    
-    #find the best matching points
-	good_new = p_new[status == 1] #status is 1 for a match
-	good_old = p_old[status == 1]
-    
+        
 	mask = np.zeros_like(frame);
     
-    #draw the overlaying tracking img
-	for i,(new,old) in enumerate(zip(good_new,good_old)):
-		a,b = new.ravel() #tmp new value
-		c,d = old.ravel() #tmp old value -- necessary for drawing line tracking
-
-		#draws a line connecting the old point with the new point
-		cv2.line(mask,(a,b),(c,d),(0,255,0),2)
-
-		#draws the new dot
-		cv2.circle(writeframe,(int(a),int(b)),4,(255,0,0),-1)
-		
-		#cv2.circle(frame, (a,b) ,10 , (0,255,255))
-
     #this is if we want to add lines into the tracking to follow the path
 	img = cv2.add(writeframe,mask)
 	writer.write(img)
     
     #update the previous frame and previous points
 	old_gray = fgray.copy()
+	index+=1
 
 #clean up
 writer.release()
